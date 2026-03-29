b21a44e3f9d3640edd3fcc55c5da3472aa151f2a
lumm2509 <hugovalco@gmail.com>
2026-03-29 14:30:29 -0700

    container: drop obsolete event aliases

--- transport/http/request_event.go
+++ transport/http/request_event.go
@@ -1,33 +1,16 @@
 package http
 
 import (
-	"database/sql"
-	"log/slog"
 	"net/http"
 	"net/netip"
-	"strings"
 	"sync"
-	"time"
 
-	"github.com/lumm2509/keel/infra/store"
 	"github.com/lumm2509/keel/pkg/inflector"
-	"github.com/lumm2509/keel/pkg/subscriptions"
 )
 
 // Common request store keys used by the middlewares and api handlers.
 const (
 	RequestEventKeyInfoContext = "infoContext"
-)
-
-const (
-	RequestInfoContextDefault       = "default"
-	RequestInfoContextExpand        = "expand"
-	RequestInfoContextRealtime      = "realtime"
-	RequestInfoContextProtectedFile = "protectedFile"
-	RequestInfoContextBatch         = "batch"
-	RequestInfoContextOAuth2        = "oauth2"
-	RequestInfoContextOTP           = "otp"
-	RequestInfoContextPasswordAuth  = "password"
 )
 
 var requestInfoPool = sync.Pool{
@@ -40,15 +23,8 @@ var requestInfoPool = sync.Pool{
 	},
 }
 
-type RequestContainer[Cradle any] interface {
-	Cradle() *Cradle
-	Logger() *slog.Logger
-	Store() *store.Store[string, any]
-	IsDev() bool
-	DataBase() *sql.DB
-	SubscriptionsBroker() *subscriptions.Broker
-}
-
+// TrustedProxyProvider can be implemented by the App type (T) to enable
+// trusted-proxy IP resolution in ClientIP.
 type TrustedProxyProvider interface {
 	TrustedProxyRanges() []netip.Prefix
 }
@@ -110,17 +86,12 @@ func (info *RequestInfo) Clone() *RequestInfo {
 }
 
 // RequestEvent defines the router handler event.
-type RequestEvent[Cradle any] struct {
-	Container         RequestContainer[Cradle]
+type RequestEvent[T any] struct {
+	App              *T
 	cachedRequestInfo *RequestInfo
 	Event
 
-	mu           sync.Mutex
-	requestID    string
-	startTime    time.Time
-	loggerBase   *slog.Logger
-	logger       *slog.Logger
-	routePattern string
+	mu sync.Mutex
 }
 
 func clearStringMap[M ~map[string]string](m M) {
@@ -135,145 +106,41 @@ func clearAnyMap[M ~map[string]any](m M) {
 	}
 }
 
-func (e *RequestEvent[Cradle]) Cradle() *Cradle {
-	return e.Container.Cradle()
-}
-
-func (e *RequestEvent[Cradle]) RequestID() string {
-	return e.requestID
-}
-
-func (e *RequestEvent[Cradle]) SetRequestID(id string) {
-	e.requestID = id
-	if e.Request != nil {
-		if id != "" {
-			e.Request.Header.Set("X-Request-ID", id)
-		} else {
-			e.Request.Header.Del("X-Request-ID")
-		}
-	}
-	if e.Response != nil && id != "" {
-		e.Response.Header().Set("X-Request-ID", id)
-	}
-	e.logger = nil
-}
-
-func (e *RequestEvent[Cradle]) StartTime() time.Time {
-	return e.startTime
-}
-
-func (e *RequestEvent[Cradle]) Duration() time.Duration {
-	if e.startTime.IsZero() {
-		return 0
-	}
-
-	return time.Since(e.startTime)
-}
-
-func (e *RequestEvent[Cradle]) Logger() *slog.Logger {
-	if e.logger != nil {
-		return e.logger
-	}
-
-	e.rebuildLogger()
-	return e.logger
-}
-
-func (e *RequestEvent[Cradle]) WithLogAttrs(args ...any) *slog.Logger {
-	return e.Logger().With(args...)
-}
-
-func (e *RequestEvent[Cradle]) ClientIP() string {
-	if provider, ok := any(e.Container).(TrustedProxyProvider); ok {
+// ClientIP returns the real client IP. If App implements TrustedProxyProvider,
+// proxy headers are used when the direct peer is in a trusted range.
+func (e *RequestEvent[T]) ClientIP() string {
+	if provider, ok := any(e.App).(TrustedProxyProvider); ok {
 		return e.RealIPFromTrustedProxies(provider.TrustedProxyRanges())
 	}
 
 	return e.RemoteIP()
 }
 
-func (e *RequestEvent[Cradle]) RoutePattern() string {
-	return e.routePattern
-}
-
-func (e *RequestEvent[Cradle]) SetStartTime(start time.Time) {
-	e.startTime = start
-}
-
-func (e *RequestEvent[Cradle]) SetLogger(logger *slog.Logger) {
-	e.loggerBase = logger
-	e.logger = nil
-}
-
-func (e *RequestEvent[Cradle]) SetRoutePattern(pattern string) {
-	if method, route, ok := strings.Cut(pattern, " "); ok && method != "" && strings.HasPrefix(route, "/") {
-		e.routePattern = route
-	} else {
-		e.routePattern = pattern
-	}
-	e.logger = nil
-}
-
-func (e *RequestEvent[Cradle]) Reset(container RequestContainer[Cradle], response http.ResponseWriter, request *http.Request) {
+func (e *RequestEvent[T]) Reset(app *T, response http.ResponseWriter, request *http.Request) {
 	e.releaseCachedRequestInfo()
-	e.Container = container
+	e.App = app
 	e.Event = Event{
 		Response: response,
 		Request:  request,
 	}
-	e.requestID = ""
-	e.startTime = time.Time{}
-	e.loggerBase = nil
-	e.logger = nil
-	e.routePattern = ""
 }
 
-func (e *RequestEvent[Cradle]) Release() {
+func (e *RequestEvent[T]) Release() {
 	e.releaseCachedRequestInfo()
-	e.Container = nil
+	e.App = nil
 	e.Event = Event{}
-	e.requestID = ""
-	e.startTime = time.Time{}
-	e.loggerBase = nil
-	e.logger = nil
-	e.routePattern = ""
 }
 
-func (e *RequestEvent[Cradle]) releaseCachedRequestInfo() {
+func (e *RequestEvent[T]) releaseCachedRequestInfo() {
 	releaseRequestInfo(e.cachedRequestInfo)
 	e.cachedRequestInfo = nil
-}
-
-func (e *RequestEvent[Cradle]) rebuildLogger() {
-	base := e.loggerBase
-	if base == nil {
-		base = slog.Default()
-		if e.Container != nil && e.Container.Logger() != nil {
-			base = e.Container.Logger()
-		}
-	}
-
-	args := make([]any, 0, 8)
-	if e.requestID != "" {
-		args = append(args, "request_id", e.requestID)
-	}
-	if e.Request != nil {
-		args = append(args, "method", e.Request.Method)
-		if e.Request.URL != nil {
-			args = append(args, "path", e.Request.URL.Path)
-		}
-	}
-	if e.routePattern != "" {
-		args = append(args, "route", e.routePattern)
-	}
-
-	e.logger = base.With(args...)
 }
 
 // RequestInfo parses the current request into RequestInfo instance.
 //
 // Note that the returned result is cached to avoid copying the request data multiple times
 // but the auth state and other common store items are always refreshed in case they were changed by another handler.
-func (e *RequestEvent[C]) RequestInfo() (*RequestInfo, error) {
+func (e *RequestEvent[T]) RequestInfo() (*RequestInfo, error) {
 	e.mu.Lock()
 	defer e.mu.Unlock()
 
@@ -282,7 +149,7 @@ func (e *RequestEvent[C]) RequestInfo() (*RequestInfo, error) {
 		if infoCtx != "" {
 			e.cachedRequestInfo.Context = infoCtx
 		} else {
-			e.cachedRequestInfo.Context = RequestInfoContextDefault
+			e.cachedRequestInfo.Context = "default"
 		}
 	} else {
 		if err := e.initRequestInfo(); err != nil {
@@ -293,10 +160,10 @@ func (e *RequestEvent[C]) RequestInfo() (*RequestInfo, error) {
 	return e.cachedRequestInfo, nil
 }
 
-func (e *RequestEvent[C]) initRequestInfo() error {
+func (e *RequestEvent[T]) initRequestInfo() error {
 	infoCtx, _ := e.Get(RequestEventKeyInfoContext).(string)
 	if infoCtx == "" {
-		infoCtx = RequestInfoContextDefault
+		infoCtx = "default"
 	}
 
 	info := acquireRequestInfo()
