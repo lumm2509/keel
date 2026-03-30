6dc252088b1c1aa85c6fa74b5852baa79d4fb9eb
lumm2509 <hugovalco@gmail.com>
2026-03-29 19:01:23 -0700

    refactor: remove deprecated container and observability modules

--- container/container.go
+++
@@ -1,5 +0,0 @@
-package container
-
-// DataDirProvider is implemented by containers that expose a data directory path.
-// Used by the HTTP serve layer to locate TLS certificate cache directories.
-type DataDirProvider interface{ DataDir() string }
